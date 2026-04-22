845985ebe2fdedc2377fb4977f782ad803e5ac0e
richard <richardcasinilloph@gmail.com>
2026-04-22 21:12:31 +0800

    wmiddleware commits

---
+++ node-mysql-api/_middleware/authorize.ts
@@ -0,0 +1,28 @@
+import { expressjwt } from 'express-jwt';
+import config from '../config.json';
+import db from '../_helpers/db';
+
+const { secret } = config;
+
+export default function authorize(roles: any = []) {
+    if (typeof roles === 'string') {
+        roles = [roles];
+    }
+
+    return [
+        expressjwt({ secret, algorithms: ['HS256'] }),
+        async (req: any, res: any, next: any) => {
+            const account = await db.Account.findByPk(req.auth.id);
+
+            if (!account || (roles.length && !roles.includes(account.role))) {
+                return res.status(401).json({ message: 'Unauthorized' });
+            }
+
+            req.user = req.auth;
+            req.user.role = account.role;
+            const refreshTokens = await account.getRefreshTokens();
+            req.user.ownsToken = (token: any) => !!refreshTokens.find((x: any) => x.token === token);
+            next();
+        }
+    ];
+}
