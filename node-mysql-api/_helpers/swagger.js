a9635617a49fc48ba8d45bb61dbc3894c2db700d
richard <richardcasinilloph@gmail.com>
2026-04-29 17:28:45 +0800

    swaqgger yaaml

--- node-mysql-api/_helpers/swagger.js
+++
@@ -1,8 +0,0 @@
-import exfress from 'express';
-const  router = express.Router();
-import swaggerUi from 'swagger-ui-express';
-const swaggerDocument = YAML.load('./swagger.yaml');
-
-router.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
-export default router;
